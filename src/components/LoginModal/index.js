964bf040c5ea4e6e1f7f948c6299ef00655c0c82
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-07-21 23:31:35 +0100

    7 - add login form and api request

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
-import { Modal, Button }  from 'react-bootstrap';
+import { Modal, Button, Form }  from 'react-bootstrap';
+import { UserService } from '../../services';
+import logo from '../../assets/images/logo.png';
 import './loginModal.scss';
 
 
 export default class LoginModal extends Component {
+
+  handleLogin(e) {
+    e.preventDefault();
+    const payload = { 
+      user: { 
+        username: e.target.email.value,
+        password: e.target.password.value 
+      }
+    };
+    UserService.doLogin(payload).then((response) => {
+      const user = response.data
+      localStorage.setItem('user', JSON.stringify(user));
+      // this.props.saveUser(user);
+      if (response.headers.authorization) {
+        localStorage.setItem('jwt', response.headers.authorization);
+        this.props.saveLogIn(localStorage.getItem('jwt'));
+      }
+      // this.props.history.push(`/`)
+    })
+    .catch((error) => {
+      switch (error.response.status) {
+        case 401:
+          alert("Password incorreta");
+          break;
+        case 404:
+          alert("Utilizador não encontrado");
+          break;
+        default: 
+          alert("Something went wrong");
+      }
+    })
+  }
 
   render() {
     const {
@@ -12,19 +46,21 @@ export default class LoginModal extends Component {
     } = this.props;
 
     return (
-      <Modal show={show} onHide={() => handleLoginClose()}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => handleLoginClose()}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={() => handleLoginClose()}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+      <Modal className='login-modal' show={show} onHide={() => handleLoginClose()}>
+        <Modal.Body>
+          <img alt='Team Maker logo' className="logo mb-3" src={logo} />
+          <Form onSubmit={(e)=> this.handleLogin(e) }>
+            <Form.Group controlId="formGroupEmail">
+              <Form.Control type="email" placeholder="Enter email" name="email" />
+            </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Control type="password" placeholder="Password" name="password"/>
+            </Form.Group>
+            <Button className='m-auto' type='submit' variant="primary">
+              Login
+            </Button>
+          </Form>
+        </Modal.Body>
       </Modal>
     )
   }
--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 /** This file serves only as an agreggator so that we can do something like 'import {x, y, x} from "./_services"' * */
 export { AuthenticationService } from './authentication';
+export { UserService } from './user';
