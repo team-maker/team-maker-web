5fd49a9d65088f31b69e3691e2768536127180f4
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-07-25 09:57:31 +0100

    17 - add logout

--- src/components/HomeNav/index.js
+++ src/components/HomeNav/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, Button }  from 'react-bootstrap';
+import { Navbar, Nav, Button, NavDropdown }  from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
-import './styles.scss';
 import LoginModal from '../LoginModal';
+import './styles.scss';
+import { UserService, AuthenticationService } from '../../services';
 import logo from '../../assets/images/logo.png';
 
 export default class HomeNav extends Component {
@@ -18,11 +19,55 @@ export default class HomeNav extends Component {
   handleLoginClose = () => this.setState({showLoginModal: false});
   handleLoginShow = () => this.setState({showLoginModal: true});
 
+  handleLogout() {
+    UserService.logOut().then((response) => {
+      AuthenticationService.logout();
+      this.setState({loggedIn: false})
+    })
+    .catch((error) => {
+      alert("Não foi possível fazer logout");
+    })
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault();
+    const payload = { 
+      username: e.target.email.value,
+      password: e.target.password.value 
+    };
+    UserService.doLogin(payload).then((response) => {
+      const user = response.data
+      localStorage.setItem('user', JSON.stringify(user));
+      // this.props.saveUser(user);
+      if (response.headers.authorization) {
+        localStorage.setItem('jwt', response.headers.authorization);
+        // this.props.saveLogIn(localStorage.getItem('jwt'));
+      }
+      this.handleLoginClose();
+      this.setState({loggedIn: user})
+      // this.props.history.push(`/`)
+    })
+    .catch((error) => {
+      switch (error.response.status) {
+        case 400:
+          alert("Email/Password incorrectos");
+          break;
+        case 404:
+          alert("Utilizador não encontrado");
+          break;
+        default: 
+          alert("Something went wrong");
+      }
+    })
+  }
+
+
   render() {
     const {
       loggedIn,
       showLoginModal
     } = this.state;
+    
     return (
       <>
         <Navbar className='home-nav p-1' collapseOnSelect expand="lg" variant="dark" fixed="top">
@@ -36,9 +81,12 @@ export default class HomeNav extends Component {
             <Nav className="ml-auto mr-4" >
               {
                 loggedIn ?
-                  <Button className="nav-button" onClick={() => this.handleLoginShow()}>
-                    {loggedIn['user']['email']}
-                  </Button>
+                  <NavDropdown title={loggedIn['user']['email']} id="basic-nav-dropdown">
+                    <NavLink className="dropdown-item" to='/profile'>
+                      Perfil
+                    </NavLink>
+                    <NavDropdown.Item onClick={() => this.handleLogout()}>Logout</NavDropdown.Item>
+                  </NavDropdown>
                  : 
                   <Button className="nav-button" onClick={() => this.handleLoginShow()}>
                     Login
@@ -48,6 +96,7 @@ export default class HomeNav extends Component {
           </Navbar.Collapse>
         </Navbar>
         <LoginModal 
+          handleLogin={this.handleLogin}
           handleLoginClose={this.handleLoginClose} 
           show={showLoginModal}
         />
