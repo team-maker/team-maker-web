9e682f20f2c6a33e6bbdf42d65fe74b3c0097882
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-08-05 23:28:21 +0100

    31 - fix deleted function

--- src/services/player.js
+++ src/services/player.js
@@ -2,6 +2,14 @@ import axios from './interceptor';
 
 export const PlayerService = {
 
+  doGetPlayer() {
+    const url ='/api/players';
+    return axios({
+      method: 'get',
+      url: url
+    });
+  },
+
   doGetTeamPlayers() {
     const url ='/api/team-players';
     return axios({
