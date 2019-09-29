a446c1d04724e3aab0d5261902d97f53cb7dcff3
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-09-29 19:31:40 +0100

    70 - add different team lineup positions

---
+++ src/components/Team/Games/Wrapper/TeamLineup/position.js
@@ -0,0 +1,100 @@
+export const FOOTBALL_7_POSITIONS = [
+  {
+    z: '280px',
+    x: '0px'
+  },
+  {
+    z: '160px',
+    x: '-100px'
+  },
+  {
+    z: '160px',
+    x: '100px'
+  },
+  {
+    z: '0px',
+    x: '-250px'
+  },
+  {
+    z: '0px',
+    x: '250px'
+  },
+  {
+    z: '-140px',
+    x: '0px'
+  },
+  {
+    z: '50px',
+    x: '0px'
+  },
+]
+
+export const FOOTBALL_5_POSITIONS = [
+  {
+    z: '280px',
+    x: '0px'
+  },
+  {
+    z: '160px',
+    x: '0px'
+  },
+  {
+    z: '50px',
+    x: '-150px'
+  },
+  {
+    z: '50px',
+    x: '150px'
+  },
+  {
+    z: '-100px',
+    x: '0px'
+  },
+]
+
+export const FOOTBALL_11_POSITIONS = [
+  {
+    z: '280px',
+    x: '0px'
+  },
+  {
+    z: '200px',
+    x: '-100px'
+  },
+  {
+    z: '200px',
+    x: '100px'
+  },
+  {
+    z: '150px',
+    x: '-250px'
+  },
+  {
+    z: '150px',
+    x: '250px'
+  },
+  {
+    z: '-140px',
+    x: '0px'
+  },
+  {
+    z: '100px',
+    x: '0px'
+  },
+  {
+    z: '0px',
+    x: '150px'
+  },
+  {
+    z: '0px',
+    x: '-150px'
+  },
+  {
+    z: '-140px',
+    x: '-150px'
+  },
+  {
+    z: '-140px',
+    x: '150px'
+  },
+]
